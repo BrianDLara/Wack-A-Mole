f6974c3558f94fdd39538c30a486203d41fd830e
Brian Lara <Briandlara1@gmail.com>
2022-09-30 18:55:28 -0400

    added an if statement that prevents the player from increasing the score after winning

--- app.js
+++ app.js
@@ -3,23 +3,27 @@ const cells = document.querySelectorAll('.cell');
 const restartButton = document.querySelector('#restart-button')
 
 let cellArray = Array.from(cells);
-let currentlyPlaying = false;
 let playerScore = document.querySelector('#player-score');
 let score = 0;
 
 const startGame = () => {
-currentlyPlaying = true;
-
-    // playerScore.innerText = score
+cells.forEach(cell => cell.addEventListener('click', function(){
+    
+    if(score < 5){
+        score += 1
+        playerScore.innerText = score 
+    }
+if(score === 5){
+        liveStatus.innerText = `You Won!`
+    }
+}))
 }
 
-const addScore = () => {
-    score += 1
-    playerScore.innerText = score
-}
 
-cells.forEach(cell => cell.addEventListener('click', addScore))
+    
+
    
+
 
 
 startGame()
