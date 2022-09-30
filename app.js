b533ce9fd5b38c6d7307fbd880b12706987dde25
Brian Lara <Briandlara1@gmail.com>
2022-09-30 18:00:08 -0400

    added a score count

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const liveStatus = document.querySelector('#live-status');
-const cell = document.querySelectorAll('.cell');
+const cells = document.querySelectorAll('.cell');
 const restartButton = document.querySelector('#restart-button')
 
-let cellArray = Array.from(cell);
-let gameInSession = false;
-let playerScore = document.querySelector('player-score')
+let cellArray = Array.from(cells);
+let currentlyPlaying = false;
+let playerScore = document.querySelector('#player-score');
+let score = 0;
+
+const startGame = () => {
+currentlyPlaying = true;
+
+    // playerScore.innerText = score
+}
+
+const addScore = () => {
+    score += 1
+    playerScore.innerText = score
+}
+
+cells.forEach(cell => cell.addEventListener('click', addScore))
+   
+
+
+startGame()
+
+
+
+
