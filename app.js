41faf4066ea6cdfbb664b8d3c70b2f2a3501948c
Brian Lara <Briandlara1@gmail.com>
2022-09-30 20:29:18 -0400

    added randomization

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
 const liveStatus = document.querySelector('#live-status');
 const cells = document.querySelectorAll('.cell');
 const restartButton = document.querySelector('#restart-button')
+const mole = document.querySelector('.mole')
 
 let cellArray = Array.from(cells);
 let playerScore = document.querySelector('#player-score');
 let score = 0;
+let time = null
 
-const startGame = () => {
-cells.forEach(cell => cell.addEventListener('click', function(){
-    
-    if(score < 5){
-        score += 1
-        playerScore.innerText = score 
-    }
-if(score === 5){
-        liveStatus.innerText = `You Won!`
-    }
-}))
+
+
+
+//functions
+const randomMole = () =>{
+    cells.forEach(cell => {
+        cell.classList.remove('mole')
+    })
+    let random = cells[Math.floor(Math.random()*5)]
+    random.classList.add('mole')
 }
 
+const moleTimer = () => {
+    time = setInterval(randomMole, 500)
+}
 
+const startGame = () => {
+    moleTimer()
     
-
-   
-
-
+    cells.forEach(cell => cell.addEventListener('click', function(){
+        if(score < 5){
+            score += 1
+            playerScore.innerText = score 
+        }
+        
+       
+        
+        
+    }))
+}
 
 startGame()
 
