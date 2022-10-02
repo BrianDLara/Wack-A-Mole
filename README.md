37aa0264c740e5d6b57de361bd1daf5983c97e1b
Brian Lara <Briandlara1@gmail.com>
2022-10-02 17:04:18 -0400

    added sound effects and costum fonts

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Wack-A-Mole
+
+##Sound Effect from <a href="https://pixabay.com/sound-effects/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=7153">Pixabay</a>
--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+//Global Variables
 const liveStatus = document.querySelector('#live-status');
 const cells = document.querySelectorAll('.cell');
 const restartButton = document.querySelector('#restart-button');
 const mole = document.querySelector('.mole');
+
+//Mole got hit audio
+const boink = new Audio('../music/boings_jews-harp-7153.mp3');
+
 
 let cellArray = Array.from(cells);
 let playerScore = document.querySelector('#player-score');
@@ -15,6 +20,26 @@ let wackMole;
 
 
 //functions
+const startGame = () => {
+    liveStatus.innerText = `Hit 10 Moles To Win`;
+    moleMoveSpeed();
+    cells.forEach(cell => {
+        cell.addEventListener('click', function(){
+            boink.play();
+            if(cell.id ===  wackMole && score < 100) { 
+                // if stopClick is true, it will return and prevent from adding more points from the same cell
+                if(stopClick){
+                    return;
+                } 
+                addScore();
+            }
+            if (score === 100){
+                youWin();
+            }
+        })
+    })
+}
+
 const randomMole = () =>{
     //revomes mole after it appears
     cells.forEach(cell => {
@@ -25,7 +50,7 @@ const randomMole = () =>{
     let random = cells[Math.floor(Math.random()*5)];
    // if the last randomized cell equals to the new random cell, it will return and run the function randomMole() again
     if(random === lastCell){
-       return randomMole();
+        return randomMole();
     }
     //adding the class .mole to the id's in the html document
     random.classList.add('mole');
@@ -47,7 +72,6 @@ const youWin = () => {
     cells.forEach(cell => {
         cell.classList.remove('mole');
     })
-
 }
 
 const addScore = () => {
@@ -56,31 +80,9 @@ const addScore = () => {
     playerScore.innerText = score;
     // stops player from cheating and adding multiple points from the same cell
     stopClick = true;
-
 }
 
-const startGame = () => {
-    liveStatus.innerText = `Hit 10 Moles To Win`;
-    moleMoveSpeed();
-    cells.forEach(cell => {
-        cell.addEventListener('click', function(){
-        if(cell.id ===  wackMole && score < 100) { 
-            // if stopClick is true, it will return and prevent from adding more points from the same cell
-            if(stopClick){
-                return;
-            } 
-            addScore();
-        }
-        
-        if (score === 100){
-            youWin();
-        }
-    })})
-    
-}
-
-
-
+//called functions
 startGame()
 
 
